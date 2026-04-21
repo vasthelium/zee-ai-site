202a281749e1ef887233be03a069115600065bd4
Syed Zameer <techiezameer.m@gmail.com>
2026-04-21 13:29:01 -0500

    CONFIG_CACHE import references fix

--- backend/app/services/db_service.py
+++ backend/app/services/db_service.py
@@ -1,28 +1,28 @@
-from app.services.db_repository import (pgconnect, init_db, create_text_embeddings_table, insert_text_embeddings, 
-connection_pool, read_stored_embeddingsdata)
+import app.services.db_repository as dbrepo
 from app.rag.embeddings import embed
 
 def sendembeddings_db():
     paired = embed()
-    conn = pgconnect()
+    conn = dbrepo.pgconnect()
     try:
-        init_db(conn)
-        create_text_embeddings_table(conn)
+        dbrepo.init_db(conn)
+        dbrepo.create_text_embeddings_table(conn)
         for items in paired:
-            insert_text_embeddings(conn, items)
+            dbrepo.insert_text_embeddings(conn, items)
         conn.commit()
         print(f"Inserted {len(paired)} records")
     finally:
-        connection_pool.putconn(conn)
+        dbrepo.connection_pool.putconn(conn)
     
 
 def read_embeddingdata():
-    conn = pgconnect()
+    conn = dbrepo.pgconnect()
     try:
-        embedding_data = read_stored_embeddingsdata(conn)
+        embedding_data = dbrepo.read_stored_embeddingsdata(conn)
     finally:
-        connection_pool.putconn(conn)
+        dbrepo.connection_pool.putconn(conn)
     return embedding_data
+
 
 if __name__ == "__main__":
     sendembeddings_db()
